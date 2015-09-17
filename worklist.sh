abaff37d7262fc7e2b46ac3d52665f5146bd104b
Michael Knowles <michael@mapledyne.com>
2015-09-16 21:29:43 -0700

    Add applescript to actually read from Evernote - more helpful with this. Generalized wordlist.sh to look up current user.

--- worklist.sh
+++ worklist.sh
@@ -5,7 +5,9 @@ iconAlert="<i class='fa fa-times-circle red'></i>"
 iconFolder="<i class='fa fa-folder-open-o'></i>"
 iconNote="<i class='fa fa-sticky-note-o'></i>"
 
-downloadDir='/Users/mknowles/Downloads'
+user=`ls -l /dev/console | awk '/ / { print $3 }'`
+
+downloadDir="/Users/${user}/Downloads"
 
 downloadCount=$(ls -l $downloadDir | sed '1d' | wc -l | tr -d ' ')
 
@@ -17,7 +19,7 @@ else
     echo "<tr><td>$iconGood $iconFolder Download folder</td><td class=green>empty</td></tr>"
 fi
 
-triageCount=$(osascript /Users/mknowles/Dropbox\ \(Personal\)/Projects\ and\ art/Applescript/triagelist.scpt)
+triageCount=$(osascript worklist.widget/triagelist.scpt)
 if [ $triageCount -gt 0 ];then
     echo "<tr><td>$iconAlert $iconNote Evernote Triage</td><td class=red>$triageCount items</td></tr>"
 else
